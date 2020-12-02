e87515e6a7f6d7fffb60f49b697daaa6988d1f91
Doreen Mensah <doreenradiance@gmail.com>
2020-12-02 09:45:54 +0000

    Created A StylesSheet

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {StyleSheet, View,} from 'react-native';
-import RedJacket from './components/RedJacket';
+import Jackets from './components/Jackets';
+import RedJacket from './RedJacket';
+import JacketsScreen from './JacketsScreen'                
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <RedJacket/> */}
+      {/* <Jackets/> */}
+      <JacketsScreen/>
     </View>
   );
 }
